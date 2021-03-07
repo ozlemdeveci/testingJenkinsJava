67b7e98f2c49451190ccd3f0184e0a60d0933e7c
ozlemdeveci <ozlemelisss@gmail.com>
2021-03-07 10:28:15 -0600

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i <6 ; i++) {
+        for (int i = 0; i <7 ; i++) {
             System.out.println("Testing");
             Thread.sleep(2000);
         }
