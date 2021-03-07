6db02b5c9fce6f7d2f5937a3775a75cc0135a63f
ozlemdeveci <ozlemelisss@gmail.com>
2021-03-07 10:05:58 -0600

    first com

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i <6 ; i++) {
+            System.out.println("Testing");
+            Thread.sleep(2000);
+        }
+    }
+}
